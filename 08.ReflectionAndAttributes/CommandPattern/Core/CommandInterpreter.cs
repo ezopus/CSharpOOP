e743f8dcd1e71338ce5f45fda062fa976d81747c
ezopus <133430790+ezopus@users.noreply.github.com>
2023-11-17 19:40:49 +0200

    reflection exercises added

---
+++ 08.ReflectionAndAttributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,35 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] arguments = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string command = arguments[0];
+
+            string[] commandArguments = arguments.Skip(1).ToArray();
+
+            Type commandType = Assembly
+                .GetEntryAssembly()
+                .GetTypes()
+                .FirstOrDefault(c => c.Name == $"{command}Command");
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Command not found");
+            }
+
+            ICommand commandInstance = Activator.CreateInstance(commandType) as ICommand;
+
+            string result = commandInstance.Execute(commandArguments);
+
+            return result.ToString();
+        }
+    }
+}
---
+++ 08.ReflectionAndAttributes/ValidationAttributes/Utils/Validator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utils
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objectType = obj.GetType();
+
+            PropertyInfo[] propertyInfo = objectType
+                .GetProperties()
+                .Where(p => p.CustomAttributes.Any(ca => typeof(MyValidationAttribute).IsAssignableFrom(ca.AttributeType)))
+                .ToArray();
+
+            foreach (PropertyInfo property in propertyInfo)
+            {
+                IEnumerable<MyValidationAttribute> attributes = property
+                    .GetCustomAttributes()
+                    .Where(ca => typeof(MyValidationAttribute).IsAssignableFrom(ca.GetType()))
+                    .Cast<MyValidationAttribute>();
+
+                foreach (MyValidationAttribute attribute in attributes)
+                {
+                    if (!attribute.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+    }
+}

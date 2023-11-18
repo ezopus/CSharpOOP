6e2ebc92f3b1b744c67547b3c655fd2305edd555
ezopus <133430790+ezopus@users.noreply.github.com>
2023-11-18 18:47:03 +0200

    unit testing lab started

--- 09.UnitTesting/Skeleton.Tests/AxeTests.cs
+++ 09.UnitTesting/Skeleton.Tests/AxeTests.cs
@@ -1,14 +1,36 @@
 using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
     [TestFixture]
     public class AxeTests
     {
-        [Test]
-        public void Test1()
-        {
+        private const int axeAttack = 10;
+        private const int axeDurability = 10;
+        private const int brokenAxeDurability = 0;
+        private const int dummyHealth = 10;
+        private const int dummyExperience = 10;
 
+        Axe axe = new Axe(axeAttack, axeDurability);
+        Axe brokenAxe = new Axe(axeAttack, brokenAxeDurability);
+        Dummy target = new Dummy(dummyHealth, dummyExperience);
+
+        [Test]
+        [TestCase(9)]
+        public void Test_CheckIfAxeLosesDurability_AfterAttack(int testNumber)
+        {
+            axe.Attack(target);
+            Assert.AreEqual(axe.DurabilityPoints, testNumber, "Axe durability has not changed.");
+        }
+
+        [Test]
+        public void Test_AttackingWithBrokenWeapon()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                brokenAxe.Attack(target);
+            }, "Broken axe throws exception");
         }
     }
 }
--- 09.UnitTesting/Skeleton.Tests/DummyTests.cs
+++ 09.UnitTesting/Skeleton.Tests/DummyTests.cs
@@ -1,14 +1,58 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
     [TestFixture]
     public class DummyTests
     {
-        [Test]
-        public void Test1()
-        {
+        private const int dummyHealth = 10;
+        private const int dummyExperience = 10;
+        private const int deadDummyHealth = 0;
+        private Axe axe = new Axe(10, 10);
 
+        Dummy aliveDummy = new Dummy(dummyHealth, dummyExperience);
+        Dummy deadDummy = new Dummy(deadDummyHealth, dummyExperience);
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            aliveDummy = new Dummy(dummyHealth, dummyExperience);
+            deadDummy = new Dummy(deadDummyHealth, dummyExperience);
+        }
+
+        [Test]
+        [TestCase(1)]
+        public void Test_DummyLosesHealth_WhenAttacked(int attack)
+        {
+            int healthBeforeAttack = dummyHealth;
+            this.aliveDummy.TakeAttack(attack);
+            Assert.AreNotEqual(aliveDummy.Health, healthBeforeAttack);
+        }
+
+        [Test]
+        [TestCase]
+        public void Test_DeadDummy_ThrowsException_WhenAttacked()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                deadDummy.TakeAttack(axe.AttackPoints);
+            }, "Dead dummy throws exception when attacked");
+        }
+
+        [Test]
+        public void Test_DeadDummy_CanGiveXP()
+        {
+            Assert.IsNotNull(deadDummy.GiveExperience(), "Dead dummy gives XP");
+        }
+
+        [Test]
+        public void Test_Dummy_CantGiveXP()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aliveDummy.GiveExperience();
+            }, "Alive dummy can't give XP");
         }
     }
 }
